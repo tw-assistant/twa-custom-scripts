153c107c0a187f32e2287b04e08ed863a6c2343a
Jonas Rafael Duarte dos Santos <jonasrdsantos@gmail.com>
2022-03-20 11:11:51 -0300

    res-sender

---
+++ res-sender.js
@@ -0,0 +1,29 @@
+// @name    Res Sender
+// @param   coord
+
+const { coord } = TWA;
+
+$.getScript("https://shinko-to-kuma.com/scripts/res-senderV2.js");
+
+window.alert = function() {
+    console.log.apply(console, arguments);
+};
+
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function automate() {
+  await delay(5000);
+  $("#coordinateTargetFirstTime").val(coord);
+  $("#saveCoord").click();
+  await delay(5000);
+  while (true) {
+    await delay(1000);
+    const button = $("#sendResources.btn")[0];
+    if (button) button.click();
+    else break;
+  }
+}
+
+automate();
