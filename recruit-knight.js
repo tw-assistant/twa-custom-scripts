b59aa02eb31bc05a7a8be605580bb60ca114c4aa
Jonas Rafael Duarte dos Santos <jonasrdsantos@gmail.com>
2023-03-10 10:07:59 -0300

    recruit paladin and knight

--- recruit-knight.js
+++ recruit-knight.js
@@ -30,18 +30,18 @@ new Promise(async (resolve, reject) => {
 
     try {
         $(".knight_recruit_launch").click();
-        await delay(1000, 300);
+        await delay(2000, 300);
 
         $("#knight_recruit_confirm").click();
-        await delay(1000, 300);
+        await delay(2000, 300);
 
         if ($("#popup_box_quest").length > 0) {
             $("#popup_box_quest .popup_box_close").click();
-            await delay(1000, 300);
+            await delay(2000, 300);
         }
 
         $(".knight_recruit_rush.btn-instant-free").click();
-        await delay(1000, 300);
+        await delay(2000, 300);
     } catch (error) { }
 
     setKnightAsRecruited();
