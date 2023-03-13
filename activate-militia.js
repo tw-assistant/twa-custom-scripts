3a7d7548927f3f7e289d864d4075dd4c4d4b3701
Jonas Rafael Duarte dos Santos <jonasrdsantos@gmail.com>
2023-03-12 22:15:24 -0300

    call militia

---
+++ activate-militia.js
@@ -0,0 +1,37 @@
+// @name Activate Militia
+// @step {gameUrl}/game.php?screen=farm
+
+function delay(time, random = 0) {
+    return new Promise(function (resolve) {
+        setTimeout(resolve, time + ~~(Math.random() * random));
+    });
+}
+
+async function main() {
+
+
+    // incomings_cell
+    const hasIncomings = $("#incomings_cell").length > 0;
+    if (!hasIncomings) return;
+
+    // #content_value .btn
+    const establishMilitiaButton = $("#content_value .btn:not(.evt-confirm)");
+    if (establishMilitiaButton) {
+        establishMilitiaButton.click(); 
+        await delay(1000);
+    }
+
+    const callMilitia = $("#content_value .evt-confirm.btn");
+    if (callMilitia) {
+        callMilitia.click();
+        await delay(1000);
+    }
+
+    const confirmYes = $(".btn-confirm-yes");
+    if (confirmYes) {
+        confirmYes.click();
+        await delay(1000);
+    }
+}
+
+main();
