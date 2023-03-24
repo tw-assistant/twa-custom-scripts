3d4dd28024c1f798283253c7b64aa8d2b107cda3
Jonas Rafael Duarte dos Santos <jonasrdsantos@gmail.com>
2023-03-24 15:31:07 -0300

    show offer value

--- check-promo-discord.js
+++ check-promo-discord.js
@@ -4,10 +4,17 @@
 
 if ($("#header_info [onclick*='Premium.buy']").length > 0) {
     const { id, name } = TribalWars.getGameData().player;
+
+    let text = null;
+    try {
+        text = $("#header_info [onclick*='Premium.buy']")[0].textContent.replaceAll('\t','').replaceAll('\n',' ').trim();
+    } catch (error) { }
+
     const message = {
-        "content": "Premium promo is available",
+        "content": text || "Premium promo is available",
         "username": `${name} (${id})`,
     }
+
     fetch(TWA.discordWebhook, {
         method: 'POST',
         headers: {
