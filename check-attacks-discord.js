6a5c0f5d95bdf030cd3e9434a82b754d1a3f9604
Jonas Rafael Duarte dos Santos <jonasrdsantos@gmail.com>
2023-03-27 08:48:11 -0300

    incomming attacks

---
+++ check-attacks-discord.js
@@ -0,0 +1,25 @@
+// @name Check Incoming Attacks and send discord message
+// @step {gameUrl}/game.php?screen=overview
+// @param(string) discordWebhook=https://discord.com/api/webhooks/...
+
+if ($("#incomings_amount").length > 0) {
+    const { id, name } = TribalWars.getGameData().player;
+
+    let incomingAmount = null;
+    try {
+        incomingAmount = parseInt($("#incomings_amount")[0].textContent.trim());
+    } catch (error) { }
+
+    const message = {
+        "content": `Incoming attacks: ${incomingAmount}`,
+        "username": `${name} (${id})`,
+    }
+
+    fetch(TWA.discordWebhook, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(message),
+    })
+}
